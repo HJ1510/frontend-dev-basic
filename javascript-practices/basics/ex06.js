a1f7831f78b1d6cec59483a68c24264738e1e81f
HJ1510 <yhj221004@gmail.com>
2022-12-13 15:56:53 +0900

    javascriptBasic

--- javascript-practices/basics/ex06.js
+++ javascript-practices/basics/ex06.js
@@ -1,33 +1,60 @@
 /*
-구문(statement)
+자바스크립트 객체 1: object 타입
+1. 자바스크립트 객체는 object타입과 function타입이 있다.
+2. 보통 function타입 객체는 '함수'라 부른다.
+3. 따라서 흔히 객체라고 부르는 것은 object타입의 객체라고 볼 수 있다.
+ */
 
-1. 구문은 실행 단위
-2. 구문의 구성요소
-    - 표현식 : 값, 변수, 연산자, 함수 호출 (표현식과 구문은 다름!)
-    - 주석
-    - 키워드
-3. 구문의 예
-    - 주석구문
-    - if(...){...}
-    - if ~ else
-    - for, while, do~while, for~in
-    - switch
-4. 세미콜론 (;)
-    - 표현식을 표현식문으로 만들어서 표현식을 실행하게 한다. (바로 실행) 구문 뒤에 세미콜론 표시할 필요x
-5. 개행 (가능한 문법적 오류가 없는 것으로 자바스크립트 엔진이 판단함)
-    1) 세미콜론 역할을 하는 경우
-    2) token을 분리하는 역할 (스페이스와 동일)
-*/
+// 생성방법 1
+// 생성자 함수를 사용
+// Object, Number, String, RegExp, Array - 내장 객체(내장 생성자 함수)
+var o1 = new Object();
+o1.name = "둘리";
+o1.age = 10;
+o1.another = new Object();
+o1.another.name = "마이콜";
+o1.another.age = 30;
 
-// 5.1) 개행의 역할 (세미콜론)
-s = "hello world";
-console.log(s)
+console.log(o1);
 
-// 5.2) 개행의 역할 (token 분리)
-a
-=
-1
-+
-2 // 여기선 세미콜론
-console.log(a)
-o = {}
+// 사용자 생성자 함수
+var MyObject = function(name) {
+    this.name = name;
+}
+var o2 = new MyObject("윤현정");
+
+console.log(o2);
+
+
+// 생성방법 2
+// {} 리터럴을 사용하는 방법
+var o3 ={};
+o3.name = "둘리";
+o3.age = 10;
+o3.another = new Object();
+o3.another.name = "마이콜";
+o3.another.age = 30;
+
+console.log(o3);
+
+// 생성방법3
+// JavaScript Object Notation JSON
+var o4 = {
+    name: "둘리",
+    age: 10,
+    anather: {
+        name: "마이콜",
+        age: 30
+    }
+};
+
+console.log(o4);
+
+// XmlHttpRequest 객체를 사용해서
+// 통신(AJAX)에서 데이터 교환 포맷으로 JSON을 사용한다
+var response = '{"name": "둘리", "email": "dooly@gmail.com"}';
+var o = JSON.parse(response);
+console.log(o);
+
+var htmls = "<li>" + o.name + ":" + o.email + "<li>";
+console.log(htmls);
