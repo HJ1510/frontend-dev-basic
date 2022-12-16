c307488df43db0a45074700de49ff74ca089b60f
HJ1510 <yhj221004@gmail.com>
2022-12-16 14:58:27 +0900

    jQuery

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,5 +1,6 @@
-var imageViewer = {	
-	images: [ 
+var imageViewer = {
+	
+	images:[ 
 		"국화:Chrysanthemum.jpg",
 		"사막:Desert.jpg",
 		"수국:Hydrangeas.jpg",
@@ -7,7 +8,8 @@ var imageViewer = {
 		"코알라:Koala.jpg",
 		"등대:Lighthouse.jpg",
 		"펭귄:Penguins.jpg",
-		"툴립:Tulips.jpg"];
+		"툴립:Tulips.jpg"],
+	
 	init: function(){
 		$(function(){
 			
